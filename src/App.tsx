ab2491e2af1549232c7ac04e15497156c61b9766
Andrei Hutsau <mwplt@mail.ru>
2022-11-12 22:04:16 +0300

    start Monday 2-nd week

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,17 @@ import './App.css';
 import {Todolist} from "./Todolist";
 
 function App() {
-    const tasks1 = [
+    const tasks = [
         {id: 1, title: "HTML&CSS1", isDone: true},
         {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "ReactJS", isDone: false},
-        {id: 4, title: "ReactJS", isDone: true},
-        {id: 5, title: "ReactJS", isDone: false}
+        {id: 3, title: "React", isDone: false},
+        {id: 4, title: "RJS", isDone: true},
+        {id: 5, title: "RctJS", isDone: false}
     ]
-    const tasks2 = [
-        {id: 1, title: "Hello world", isDone: true},
-        {id: 2, title: "I am Happy", isDone: false},
-        {id: 3, title: "Yo", isDone: false}
-    ]
-
 
     return (
         <div className="App">
-            <Todolist title={"What to learn"} tasks={tasks1}/>
-            <Todolist title={"What to buy"} tasks={tasks2}/>
+            <Todolist title={"What to learn"} tasks={tasks}/>
         </div>
     );
 }
