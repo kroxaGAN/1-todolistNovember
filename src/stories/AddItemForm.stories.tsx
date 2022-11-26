78ea3fe82718cb8c1b73544bef7a336030e3c760
Andrei Hutsau <mwplt@mail.ru>
2022-11-26 22:33:02 +0300

    wensdey day 4 StoryBook for AddItemForm

--- src/stories/AddItemForm.stories.tsx
+++ src/stories/AddItemForm.stories.tsx
@@ -1,41 +1,73 @@
-import React from 'react';
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button } from './Button';
+import {AddItemForm} from "../components/AddItemForm";
+import {action} from "@storybook/addon-actions";
+import {TextField} from "@mui/material";
+import {ButtonUniver} from "../components/ButtonUniver";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
-  component: Button,
+  title: 'TODOLISTS/AddItemFormExample',
+  component: AddItemForm,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    callback: {
+      description:'Button clicked in form'
+    },
   },
-} as ComponentMeta<typeof Button>;
+} as ComponentMeta<typeof AddItemForm>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof AddItemForm> = (args) => <AddItemForm {...args} />;
+const TemplateWithError: ComponentStory<typeof AddItemForm> = (args) => {
+  console.log('AddItemForm')
+  const [inputValue, setInputValue] = useState('')
+  const [error,setError]=useState<string | null>('Title is requered')
+  const addTaskHandler = () => {
+    if (inputValue.trim()!==''){
+      args.callback(inputValue.trim())
+      setInputValue('')
+    } else {
+      setError('Title is requered')
+    }
 
-export const Primary = Template.bind({});
+  }
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setError(null)
+    setInputValue(event.currentTarget.value)
+  }
+  const onKeyPressHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      addTaskHandler()
+    }
+  }
+
+  return(
+      <div>
+        <TextField
+            id="outlined-basic"
+            label="title"
+            variant="outlined"
+            value={inputValue}
+            onChange={onChangeHandler}
+            onKeyPress={onKeyPressHandler}
+            error={!!error}
+            helperText={error}
+        />
+        <ButtonUniver title={args.title} callback={addTaskHandler}/>
+
+      </div>
+  )
+};
+
+export const AddItemFormStory = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
+AddItemFormStory.args = {
+  callback: action('Button clicked in form')
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+export const AddItemFormStoryWithError = TemplateWithError.bind({});
+AddItemFormStoryWithError.args = {
+  callback: action('Button clicked in form')
 };
